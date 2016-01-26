7f9e3e673d30c78ee05d3813cd66954e038c4fa4
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-01-26 11:25:13 -0800

    Release 0.2.0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup
 
 setup(
     name='climax',
-    version='0.1.0',
+    version='0.2.0',
     url='http://github.com/miguelgrinberg/climax/',
     license='MIT',
     author='Miguel Grinberg',
