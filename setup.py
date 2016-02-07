bb2c90ecf67373f2f414d4934f15498e190d159a
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-02-07 12:41:40 -0800

    Corrected classifiers in setup.py

--- setup.py
+++ setup.py
@@ -26,12 +26,11 @@ setup(
     ],
     test_suite='test_climax',
     classifiers=[
-        'Environment :: Web Environment',
+        'Environment :: Console',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
 )
